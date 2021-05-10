330fedd3fe7608c3bd039672ea159e2e652edb07
Salomon Aengenheyster-Aber <salomon@obskura.xyz>
2021-05-10 18:11:13 +0100

    Configured visuals

--- config.def.h
+++ config.def.h
@@ -5,17 +5,18 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Liberation Mono:size=10:dpi=220:antialias=true" };
+static const char dmenufont[]       = "Liberation Mono:size=10:dpi=220:antialias=true";
+static const char col_nrm_fg[]       = "#555555";
+static const char col_nrm_bg[]       = "#000000";
+static const char col_nrm_bd[]       = "#444444";
+static const char col_sel_fg[]       = "#ffffff";
+static const char col_sel_bg[]       = "#000000";
+static const char col_sel_bd[]       = "#999999";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_nrm_fg, col_nrm_bg, col_nrm_bd },
+	[SchemeSel]  = { col_sel_fg, col_sel_bg, col_sel_bd  },
 };
 
 /* tagging */
@@ -44,7 +45,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -56,7 +57,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_nrm_bg, "-nf", col_nrm_fg, "-sb", col_sel_bg, "-sf", col_sel_fg, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
